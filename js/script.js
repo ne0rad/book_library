d7bab073b43b7bf023637e3466e67dc844d9f225
ne0rad <usackasr@gmail.com>
2021-09-14 17:32:38 +0100

    Added the ability to add books

--- js/script.js
+++ js/script.js
@@ -10,50 +10,74 @@ function Book(id, title, author, pages, read) {
 
 function addBookToLibrary(title, author, pages, read) {
     myLibrary.push(new Book(myLibrary.length, title, author, pages, read));
+    generateCard(title, author, pages, read);
 }
 
 function removeFromLibrary(id) {
     const index = myLibrary.findIndex(Book => Book.id === id);
     myLibrary.splice(index, 1);
     document.getElementById(id).remove();
-    console.log(myLibrary);
 }
 
-function populateCards() {
+function closeAddWindow() {
+    document.getElementById("addBook").style = "opacity: 0; z-index: -1";
+}
+function openAddWindow() {
+    document.getElementById("addBook").style = "opacity: 1; z-index: 0";
+}
+
+function addBookClick() {
+    let title = document.getElementById("titleInput");
+    let author = document.getElementById("authorInput");
+    let pages = document.getElementById("pagesInput");
+    let read = document.getElementById("readInput");
+    addBookToLibrary(title.value, author.value, pages.value, read.checked);
+    closeAddWindow();
+
+    title.value = "";
+    author.value = "";
+    pages.value = "";
+    read.checked = false;
+}
+
+function generateCard(title, author, pages, read) {
     let main = document.getElementById("main");
-    myLibrary.forEach(book => {
-        let card = main.appendChild( document.createElement("div"));
-        card.className = "bookCard";
-        card.id = book.id;
 
-        let bookDescription = card.appendChild( document.createElement("div") );
-        bookDescription.className = "cardContent";
+    let card = main.appendChild( document.createElement("div"));
+    card.className = "bookCard";
+    card.id = myLibrary.length;
 
-        let title = bookDescription.appendChild( document.createElement("span") );
-        title.innerHTML = book.title;
-        bookDescription.appendChild( document.createElement("br") );
+    let bookDescription = card.appendChild( document.createElement("div") );
+    bookDescription.className = "cardContent";
 
-        let author = bookDescription.appendChild( document.createElement("span") );
-        author.innerHTML = book.author;
-        bookDescription.appendChild( document.createElement("br") );
+    let titleText = bookDescription.appendChild( document.createElement("span") );
+    titleText.innerHTML = "Title: " + title;
+    bookDescription.appendChild( document.createElement("br") );
 
-        let pages = bookDescription.appendChild( document.createElement("span") );
-        pages.innerHTML = book.pages;
-        bookDescription.appendChild( document.createElement("br") );
+    let authorText= bookDescription.appendChild( document.createElement("span") );
+    authorText.innerHTML = "Author: " + author;
+    bookDescription.appendChild( document.createElement("br") );
 
-        let read = bookDescription.appendChild( document.createElement("span") );
-        read.innerHTML = book.read ? "Status: read" : "Status: not read";
+    let pagesText = bookDescription.appendChild( document.createElement("span") );
+    pagesText.innerHTML = "Pages: " + pages;
+    bookDescription.appendChild( document.createElement("br") );
 
-        let buttons = card.appendChild( document.createElement("div") );
-        let editBtn = buttons.appendChild( document.createElement("button") );
-        let delBtn = buttons.appendChild( document.createElement("button") );
-        editBtn.innerHTML = "Edit";
-        editBtn.className = "btn edit";
-        delBtn.innerHTML = "Delete";
-        delBtn.className = "btn delete";
-        delBtn.addEventListener("click", () => removeFromLibrary(book.id));
-    });
+    let readText = bookDescription.appendChild( document.createElement("span") );
+    readText.innerHTML = read ? "Status: read" : "Status: not read";
+
+    let buttons = card.appendChild( document.createElement("div") );
+    let editBtn = buttons.appendChild( document.createElement("button") );
+    let delBtn = buttons.appendChild( document.createElement("button") );
+    editBtn.innerHTML = "Edit";
+    editBtn.className = "btn edit";
+    delBtn.innerHTML = "Delete";
+    delBtn.className = "btn delete";
+    delBtn.addEventListener("click", () => removeFromLibrary(card.id));
 }
+
+document.getElementById("addCancel").addEventListener("click", () => closeAddWindow());
+document.getElementById("addNewBtn").addEventListener("click", () => openAddWindow());
+document.getElementById("addAdd").addEventListener("click", () => addBookClick());
 
 addBookToLibrary("Asadads","Mr Asd", 222, false);
 addBookToLibrary("The Lord of Flies","William Golding", 224, true);
@@ -61,5 +85,3 @@ addBookToLibrary("The Lord of Flies","William Golding", 224, true);
 for (let i = 0; i < 10; i++) {
     addBookToLibrary("Very nice title","Best author ever", i, false)
 }
-
-populateCards();
