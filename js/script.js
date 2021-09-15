5646cd5ce9287167b785d101b13d0f962962e6dd
ne0rad <usackasr@gmail.com>
2021-09-15 09:08:13 +0100

    Formating, comments

--- js/script.js
+++ js/script.js
@@ -1,32 +1,41 @@
 let myLibrary = [];
 
-function Book(id, title, author, pages, read) {
-    this.id = id;
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
+class Book {
+    constructor(id, title, author, pages, read) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
+    }
 }
 
 function addBookToLibrary(title, author, pages, read) {
-    myLibrary.push(new Book(myLibrary.length, title, author, pages, read));
+    // Take the last book from array and set index to id+1, else set index to 1
+    // Breaks if you sort an array, only works if last added book is the last item in the array :(
+    const bookIndex = myLibrary.length > 0 ? myLibrary[myLibrary.length - 1].id + 1 : 1;
     generateCard(title, author, pages, read);
+    myLibrary.push(new Book(bookIndex, title, author, pages, read));
 }
 
 function removeFromLibrary(id) {
+    // Find matching id for the book to remove
     const index = myLibrary.findIndex(Book => Book.id === id);
     myLibrary.splice(index, 1);
     document.getElementById(id).remove();
 }
 
 function closeAddWindow() {
+    // Hide window for adding a new book
     document.getElementById("addBook").style = "opacity: 0; z-index: -1";
 }
 function openAddWindow() {
+    // Show window for adding a new book
     document.getElementById("addBook").style = "opacity: 1; z-index: 0";
 }
 
 function addBookClick() {
+    // Take values from inputs and passes them to a function which adds a new book
     let title = document.getElementById("titleInput");
     let author = document.getElementById("authorInput");
     let pages = document.getElementById("pagesInput");
@@ -34,6 +43,7 @@ function addBookClick() {
     addBookToLibrary(title.value, author.value, pages.value, read.checked);
     closeAddWindow();
 
+    // Reset input fields
     title.value = "";
     author.value = "";
     pages.value = "";
@@ -41,47 +51,57 @@ function addBookClick() {
 }
 
 function generateCard(title, author, pages, read) {
+
+    // Generating HTML with Vanilla JS, yay...
+
     let main = document.getElementById("main");
 
-    let card = main.appendChild( document.createElement("div"));
+    let card = main.appendChild(document.createElement("div"));
     card.className = "bookCard";
-    card.id = myLibrary.length;
+    // Take the last book from array and set index to id+1, if array is empty set index to 1
+    // Breaks if you sort an array, only works if last added book is the last item in the array :(
+    card.id = myLibrary.length > 0 ? myLibrary[myLibrary.length - 1].id + 1 : 1;
 
-    let bookDescription = card.appendChild( document.createElement("div") );
+    let bookDescription = card.appendChild(document.createElement("div"));
     bookDescription.className = "cardContent";
 
-    let titleText = bookDescription.appendChild( document.createElement("span") );
+    let titleText = bookDescription.appendChild(document.createElement("span"));
     titleText.innerHTML = "Title: " + title;
-    bookDescription.appendChild( document.createElement("br") );
+    bookDescription.appendChild(document.createElement("br"));
 
-    let authorText= bookDescription.appendChild( document.createElement("span") );
+    let authorText = bookDescription.appendChild(document.createElement("span"));
     authorText.innerHTML = "Author: " + author;
-    bookDescription.appendChild( document.createElement("br") );
+    bookDescription.appendChild(document.createElement("br"));
 
-    let pagesText = bookDescription.appendChild( document.createElement("span") );
+    let pagesText = bookDescription.appendChild(document.createElement("span"));
     pagesText.innerHTML = "Pages: " + pages;
-    bookDescription.appendChild( document.createElement("br") );
+    bookDescription.appendChild(document.createElement("br"));
 
-    let readText = bookDescription.appendChild( document.createElement("span") );
+    let readText = bookDescription.appendChild(document.createElement("span"));
     readText.innerHTML = read ? "Status: read" : "Status: not read";
 
-    let buttons = card.appendChild( document.createElement("div") );
-    let editBtn = buttons.appendChild( document.createElement("button") );
-    let delBtn = buttons.appendChild( document.createElement("button") );
+    let buttons = card.appendChild(document.createElement("div"));
+    let editBtn = buttons.appendChild(document.createElement("button"));
+    let delBtn = buttons.appendChild(document.createElement("button"));
     editBtn.innerHTML = "Edit";
     editBtn.className = "btn edit";
     delBtn.innerHTML = "Delete";
     delBtn.className = "btn delete";
+    // Event listeners for each button on the card
     delBtn.addEventListener("click", () => removeFromLibrary(card.id));
 }
 
+
+// Event Listeners
 document.getElementById("addCancel").addEventListener("click", () => closeAddWindow());
 document.getElementById("addNewBtn").addEventListener("click", () => openAddWindow());
 document.getElementById("addAdd").addEventListener("click", () => addBookClick());
 
-addBookToLibrary("Asadads","Mr Asd", 222, false);
-addBookToLibrary("The Lord of Flies","William Golding", 224, true);
+
+// Add some random books for testing
+addBookToLibrary("Asadads", "Mr Asd", 222, false);
+addBookToLibrary("The Lord of Flies", "William Golding", 224, true);
 
 for (let i = 0; i < 10; i++) {
-    addBookToLibrary("Very nice title","Best author ever", i, false)
+    addBookToLibrary("Very nice title", "Best author ever", i, false)
 }
